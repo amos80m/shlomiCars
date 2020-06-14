e1b462a4366d1150dc7df683545b441eb17fa0cc
Amos Megidish <amosos1980@gmail.com>
2020-06-14 19:22:15 +0300

    fp

---
+++ src/screens/dashboard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container, CssBaseline, Typography,Button} from '@material-ui/core';
+
+const Dashboard = () => <React.Fragment>
+<CssBaseline />
+<Container maxWidth="false">
+  <Typography component="div">
+  
+      </Typography>
+</Container>
+</React.Fragment>;
+
+export default Dashboard;
